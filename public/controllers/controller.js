96d66140b891a1e35df63714575fba99116da40f
xmarianF <xmarianF@gmail.com>
2016-12-02 00:28:12 +0100

    PUT & DELETE Events

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,23 +1,48 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
+	console.log("Hello World from controller");
 
 var refresh = function() {
-  $http.get('/events').success(function(response) {
-    console.log("I got the data I requested");
-    $scope.events = response;
-    $scope.event = "";
-  });
+	$http.get('/events').success(function(response) {
+		console.log("I got the data I requested");
+		$scope.events = response;
+		$scope.event = "";
+	});
 };
 
 refresh();
 
 $scope.addEvent = function() {
-  console.log($scope.event);
-  $http.post('/events', $scope.event).success(function(response) {
-    console.log(response);
-    refresh();
-  });
+	console.log($scope.event);
+	$http.post('/events', $scope.event).success(function(response) {
+		console.log(response);
+		refresh();
+	});
 };
+
+$scope.removeEvent = function(id) {
+	console.log(id);
+	$http.delete('/events/' + id).success(function(response) {
+		refresh();
+	});
+};
+
+$scope.editEvent = function(id) {
+	console.log(id);
+	$http.get('/events/' + id).success(function(response) {
+		$scope.event = response;
+	});
+};
+
+$scope.updateEvent = function() {
+	console.log($scope.event._id);
+	$http.put('/events/' + $scope.event._id, $scope.event).success(function(response) {
+		refresh();
+	});
+};
+
+$scope.deselect = function() {
+	$scope.event = "";
+}
 
 }]);﻿
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
+// Wyświetlenie wydarzeń
 app.get('/events', function (req, res) {
 	console.log('I received a GET request');
 
@@ -16,12 +17,54 @@ app.get('/events', function (req, res) {
 	});
 });
 
+// Dadanie wydarzenia
 app.post('/events', function (req, res) {
 	console.log(req.body);
 
 	db.events.insert(req.body, function(err, doc) {
 		res.json(doc);
 	});
+});
+
+// Usunięcie wydarzenia
+app.delete('/events/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(id);
+	
+	db.events.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+		res.json(doc);
+	});
+});
+
+// Wyświetlenie pojedyńczego wydarzenia
+app.get('/events/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(id);
+
+	db.events.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+		res.json(doc);
+	});
+});
+
+// Modyfikacja pojedyńczego wydarzenia
+app.put('/events/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(req.body.name);
+
+	db.events.findAndModify({
+		query: {_id: mongojs.ObjectId(id)},
+		update: {$set: 
+			{
+				name: req.body.name,
+				category: req.body.category,
+				place: req.body.place,
+				date: req.body.date
+			}
+		},
+		new: true}, function (err, doc) {
+			res.json(doc);
+		}
+	);
 });
 
 app.listen(3000);
