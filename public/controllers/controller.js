e229ed9b223187e4d7b657130cb8b8ef7a653dfc
xmarianF <xmarianF@gmail.com>
2016-12-01 22:34:05 +0100

    Connecting with database

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,15 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
-}]);
+
+var refresh = function() {
+  $http.get('/events').success(function(response) {
+    console.log("I got the data I requested");
+    $scope.events = response;
+    $scope.event = "";
+  });
+};
+
+refresh();
+
+}]);﻿
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('events', ['events']);
 
 app.use(express.static(__dirname + "/public"));
+
+app.get('/events', function (req, res) {
+  console.log('I received a GET request');
+
+  db.events.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+
 app.listen(3000);
 console.log("server running on port 3000");
