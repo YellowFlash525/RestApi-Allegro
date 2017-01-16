fd877144d529973909e5661bbf8306eb61062e88
xmarianF <xmarianF@gmail.com>
2017-01-16 19:27:53 +0100

    Swagger - correction

--- server.js
+++ server.js
@@ -39,3 +39,14 @@ app.use('/apirest', routeUsers);
 
 app.listen(port);
 console.log("server running on port " + port);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use('/', express.static(__dirname));
+app.get('/swagger.json', function(req, res) {
+ res.sendFile(__dirname + '/public/swagger.json');
+});
